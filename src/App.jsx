c1ed525ff6fe4ca9e2cb666d021f4490dc67802f
kishankumarhs <hskishanraj@gmail.com>
2024-04-06 02:55:33 +0530

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { BrowserRouter,Route ,Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import ContactUs from "./pages/Contact";
+import AboutUs from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Login />} />
-     </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
